d3b85db53e1d45ac6ad3274f90d45c4f95ad1fb9
Johnfet <99792694+Johnfet@users.noreply.github.com>
2024-12-19 16:48:47 +0200

    Implemented unit changes across the code and finished feedforward controller design

--- A2.m
+++ A2.m
@@ -1,8 +1,6 @@
-
+%% Initializing the process
 clear;
-
 load MSD2024_P2_Plant.mat % Load the file
-
 s = tf('s');
 % Define options for Bode plot
 opts = bodeoptions;
@@ -14,118 +12,93 @@ opts = bodeoptions;
 % title('System Response');
 
 %% Feedback Controller tuning
-wc = 16.07; %radians
-
+% Shapeit parameters
 Kp = 0.5;
+fi = 230; %Hz
+fd =  130 ; %Hz
+ft = 90000; %Hz
+% Convert to rad/s
+wi = fi*2*pi; %Integrator zero, rad/s
+wd = fd*2*pi; %Differentiator pole, rad/s
+wt = ft*2*pi; %(Tamed)Differentiator zero, rad/s
+%Create PID transfer function
+C_PID = (Kp + wi/s + ((s/wd) + 1) / ((s/wt) + 1));      %proportional, integral, lead/lag
 
-wi = 230; %Hz, must be changed
-
-wd =  130 ; %Hz, must be changed
-
-wt = 90000; %Hz, must be changed
-
-C_PID = (Kp + wi/s + ((s/wd) + 1) / ((s/wt) + 1))      %proportional, integral, lead/lag
-
-
-% w11 = 738;
-% zeta_11 = 0.01 ;
-% w12 = 738;
-% zeta_12 = 0.9 ;
-% Q11 = 1/(2*zeta_11);
-% Q12 = 1/(2*zeta_12);
-% notch = ((s/w11)^2 + (s/(Q11*w11)) + 1) / ((s/w12)^2 + (s/(Q12*w12)) + 1);
-
-w1 = 738;           % Notch frequency (rad/s)
+% Regular notch filter implementation
+f1 = 738;           % Notch frequency (Hz)
 zeta_1 = 0.01;      % Damping for numerator (sharp)
 zeta_2 = 0.9;       % Damping for denominator (wider)
+% Convert to rad/s
+w1 = f1*2*pi; %rad/s
+% Create transfer function
 num_notch = [1, 2*zeta_1*w1, w1^2];  % Numerator coefficients
 den_notch = [1, 2*zeta_2*w1, w1^2];  % Denominator coefficients
 notch = tf(num_notch, den_notch);
 
-
-% w21 = 1009;
-% zeta_21 = 0.01;
-% w22 = 971.7;
-% zeta_22 = 0.005;
-% Q21 = 1/(2*zeta_21);
-% Q22 = 1/(2*zeta_22);
-% skewed_notch = ((2/w21)^2 + (s/(Q21*w21)) + 1) / ((s/22)^2 + (s/(Q22*w22)) + 1);
-
-
-w21 = 1009;          % Frequency for numerator (rad/s)
+% Skewed notch implementation
+f21 = 1009;          % Frequency for numerator (Hz)
 zeta_21 = 0.01;      % Damping ratio for numerator
-w22 = 971.7;         % Frequency for denominator (rad/s)
+f22 = 971.7;         % Frequency for denominator (Hz)
 zeta_22 = 0.005;     % Damping ratio for denominator
+% Convert to rad/s
+w21 = f21*2*pi; %Notch zero, rad/s
+w22 = f22*2*pi; %Notch pole, rad/s
+% Create transfer function
 num_skewed = [1, 2*zeta_21*w21, w21^2];  % Numerator coefficients
 den_skewed = [1, 2*zeta_22*w22, w22^2];  % Denominator coefficients
-
 skewed_notch = tf(num_skewed, den_skewed);
-
-
-Q21 = 1/(2*zeta_21);
-Q22 = 1/(2*zeta_22);
-
-notch2 = ((2/w21)^2 + (s/(Q21*w21)) + 1) / ((s/22)^2 + (s/(Q22*w22)) + 1);
 
 %construct controller
 C = C_PID * notch * skewed_notch;
 
-bode(C);
-
-% figure;
-% bode(C); grid on;
-% title('Controller Response');
+figure;
+bode(C); grid on;
+title('Controller Response');
 
 %% Open loop 
-% 
-% disp(opts);
-% 
-% L = G*C;
-% poles = pole(L);
-% disp(poles);
-% figure;
-% bodeplot(L, opts); 
-% grid on;
-% margin(L);
-% title('Open Loop Response');
 
-% %% Sensitivity stuff
-% T = L / (1 + L);
-% S = 1 / (1 + L);
+disp(opts);
 
-% 
-% figure;
-% bode(T); grid on;
-% margin(T);
-% title('Complimentary Sensitivity');
-% 
-% figure;
-% bode(S); grid on;
-% margin(S);
-% title('Sensitivity');
+L = G*C;
+poles = pole(L);
+disp(poles);
+figure;
+bodeplot(L, opts); 
+grid on;
+margin(L);
+title('Open Loop Response');
 
+%% Closed loop transfer functions
+T = L / (1 + L);
+S = 1 / (1 + L);
+
+
+figure;
+bode(T); grid on;
+margin(T);
+title('Complimentary Sensitivity');
+
+figure;
+bode(S); grid on;
+margin(S);
+title('Sensitivity');
+% Might have to add nyquist plots to evaluate modulus margin here!!!
 
 %% Step response
-G.u='u'; G.y='x';
-C.u='e'; C.y='v';
+G.u='v'; G.y='x'; % Caution: Giannis changed u with v and vice versa according to lecture 7
+C.u='e'; C.y='u'; 
 
 S1 = sumblk("e = r - y");
-S2 = sumblk("u = d + v");
+S2 = sumblk("v = d + u");
 S3 = sumblk("y = x + n");
 
 Tsum = connect(G,C,S1,S2,S3,"r","y");
 
-% 
-% figure;
-% step(Tsum); grid on;
-% title('Closed Loop Step Response');
 
-% info = stepinfo(Tsum);
-% 
-% info
-
-%End of Closed Loop Controller
-
+figure;
+step(Tsum); grid on;
+title('Closed Loop Step Response (Tsum)');
+info = stepinfo(Tsum)
 
 %% Feedforward Controller tuning
 % New controller parameters for ff
@@ -159,15 +132,14 @@ C_notch = ((s/w2)^2+s/(Qz2*w2)+1)/((s/w2)^2+s/(Qp2*w2)+1);
 C_lowpass = 1/(s/wlp+1);
 Cff = C_skewednotch*C_notch*C_lowpass;
 
-% figure;
-% bode(F); grid on;
-% title('Feedforward Controller Response');
+figure;
+bode(Cff); grid on;
+title('Feedforward Controller Response');
 
-
-disp(isproper(C));
+if isproper(C)
+    disp('Feedforward controller is proper! Check response for strictly proper')
+end
 %IS PROPER VERY NICE! (Borat voice)
-% wclp = 2 * wc;
-% LowPass = 1 / (((s / wclp) + 1)^2)    ;
 
 % disp(LowPass);
 % LowPass
