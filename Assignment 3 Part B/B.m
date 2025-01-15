70e5c963e19ae2ad375f8bdbe031b6bfc15addb6
Johnfet <99792694+Johnfet@users.noreply.github.com>
2025-01-15 17:30:31 +0100

    Fixed some errors with the handling of frequencies

--- Assignment 3 Part B/B.m
+++ Assignment 3 Part B/B.m
@@ -1,3 +1,5 @@
+close all
+clear
 %% Setup parameters and inputs
 load MSD2024_P3_Signals.mat
 load MSD2024_P2_Plant.mat
@@ -21,20 +23,30 @@ ylabel("Signal")
 hold off
 grid on
 
-% Plot signals in frequency domain
-fft_d = fft(d);                 % Obtain the amplitude of d
-fft_n = fft(n);                 % Obtain the amplitude of n
-f_vec = (0:N-1)/(N*ts);         % Obtain frequency vector
+% Transform signals to the frequency domain
+fft_d_double = fft(d)/N;            % Obtain the amplitude of d, scale by N
+fft_n_double = fft(n)/N;            % Obtain the amplitude of n, scale by N
+f_vec = (0:N-1)/(N*ts);             % Obtain frequency vector (unshifted)
+f_vec_p = f_vec(1:N/2);             % Frequency vector for positive frequencies only
+% Obtain single-sided spectrum
+fft_d = zeros(1,N/2);
+fft_n = zeros(1,N/2);
+% Accumulate to positive frequencies
+fft_d = 2*fft_d_double(1:N/2);
+fft_n = 2*fft_n_double(1:N/2);
+fft_d(1) = fft_d_double(1);         % DC is excluded
+fft_n(1) = fft_n_double(1);         % DC is excluded
 
+% Plot signals in frequency domain
 figure;
-semilogx(f_vec,abs(fft_n),'b',"LineWidth",2)
+semilogx(f_vec_p,abs(fft_n),'b',"LineWidth",2)
 title("Discrete Fourier Transform of Output Disturbance")
 xlabel("f (Hz)")
 ylabel("|n| (\mum)")
 grid on
 
 figure;
-semilogx(f_vec,abs(fft_d),'r',"LineWidth",2)
+semilogx(f_vec_p,abs(fft_d),'r',"LineWidth",2)
 title("Discrete Fourier Transform of Process Disturbance")
 xlabel("f (Hz)")
 ylabel("|d| (V)")
@@ -42,18 +54,18 @@ grid on
 
 %% B2
 % Compute single-sided PSD
-psd_d = (abs(fft_d).^2)/(N*fs);         % Single-sided PSD
-psd_n = (abs(fft_n).^2)/(N*fs);    % Single-sided PSD
+psd_d = (abs(fft_d).^2)/(N*fs/2);    % Single-sided PSD
+psd_n = (abs(fft_n).^2)/(N*fs/2);    % Single-sided PSD
 % Plot the results
 figure;
-semilogx(f_vec,psd_d,'r',"LineWidth",2);
+semilogx(f_vec_p,psd_d,'r',"LineWidth",2);
 title("PSD of Process Disturbance")
 xlabel("f (Hz)")
 ylabel("PSD (V^2/Hz)")
 grid on
 
 figure;
-semilogx(f_vec,psd_n,'b',"LineWidth",2);
+semilogx(f_vec_p,psd_n,'b',"LineWidth",2);
 title("PSD of Output Disturbance")
 xlabel("f (Hz)")
 ylabel("PSD (\mum^2/Hz)")
@@ -66,51 +78,40 @@ P = G;                                  % Define plant tf as P
 GS = P/(1+P*C);                         % Process sensitivity
 S = 1/(1+P*C);                          % Output sensitivity
 % Calculate function frequency response
-[A_GS,f_GS] = freqresp(GS,f_vec,'Hz'); % Freqresponse for GS
-A_GS = squeeze(A_GS)';                          % Remove empty dimensions
-[A_S,f_S] = freqresp(S,f_vec,'Hz');    % Freqresponse for S
-A_S = squeeze(A_S)';                            % Remove empty dimensions
+[A_GS,f_GS] = freqresp(GS,f_vec_p,'Hz');    % Freqresponse for GS
+A_GS = squeeze(A_GS)';                      % Remove empty dimensions
+[A_S,f_S] = freqresp(S,f_vec_p,'Hz');       % Freqresponse for S
+A_S = squeeze(A_S)';                        % Remove empty dimensions
 % Calculate PSD of y
-psd_y = abs(A_GS).^2.*psd_d + abs(A_S).^2.*psd_n;   % PSD of y
+psd_y = ((abs(A_GS).^2).*psd_d) + ((abs(A_S).^2).*psd_n);   % PSD of y, the result of this does not make sense
 % Plot PSD of y
 figure;
-loglog(f_vec,psd_y,'k',"LineWidth",2);     % Use log on both axes due to large variations
+semilogx(f_vec_p,psd_y,'k',"LineWidth",2);
 title("PSD of Output y")
 xlabel("f (Hz)")
 ylabel("PSD (\mum^2/Hz)")
 grid on
-
+figure
+loglog(f_vec_p,abs(A_S))
+title("Response of S")
+figure
+loglog(f_vec_p,abs(A_GS))
+title("Response of GS")
 %% B4
 % Calculate Cumulative Power Spectrum
-cps_y = cumsum(psd_y.*f_vec(2));
+cps_y = cumsum(psd_y.*f_vec_p(2));
+cps_d = cumsum(psd_d.*f_vec_p(2));
+cps_n = cumsum(psd_n.*f_vec_p(2));
 % Plot PSD of y
 figure;
-loglog(f_vec,cps_y,'k',"LineWidth",2);  % Use log on both axes due to large variations
-title("CPS of Output y")
+loglog(f_vec_p,cps_y,'k',"LineWidth",2);
+hold on
+loglog(f_vec_p,cps_d,'r',"LineWidth",2);
+hold on
+loglog(f_vec_p,cps_n,'b',"LineWidth",2);
+hold off
+title("CPS")
 xlabel("f (Hz)")
 ylabel("CPS (\mum^2)")
+legend('Output','Process Disturbance','Output Disturbance')
 grid on
-% Calculate complimentary sensitivity
-T = P*C/(1+P*C);                        % tf format
-[A_T,f_T] = freqresp(T,f_vec,'Hz');     % Freqresponse for T
-A_T = squeeze(A_T)';                    % Remove empty dimensions
-% Error contributions
-e_d = abs(A_GS).*d;                     % Process disturbance error
-e_n = abs(A_T).*n;                      % Output disturbance error
-e_total = sqrt(e_d.^2+e_n.^2);            % Total error
-% Convert error performance from time to frequency domain
-E_d = fft(e_d);                         % Discrete Fourier Tranform of e_d
-E_n = fft(e_n);                         % Discrete Fourier Tranform of e_n
-E_total = fft(e_total);                 % Discrete Fourier Tranform of e_total
-% Plot error contributions
-figure;
-plot(f_vec,E_total,'k',"LineWidth",2);
-hold on
-plot(f_vec,E_d,'r',"LineWidth",2);
-hold on
-plot(f_vec,E_n,'b',"LineWidth",2);
-hold off
-title("Error of Output y")
-xlabel("f (Hz)")
-ylabel("e (\mum^2)")
-legend('Total error','Process Disturbance error','Output Disturbance error')
