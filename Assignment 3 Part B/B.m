ac8f5509a996db0cc959c6c3fc2cce02c3ecae5f
Johnfet <99792694+Johnfet@users.noreply.github.com>
2025-01-17 18:28:42 +0100

    Found and fixed the error that made the CPS contributions larger than the result. See line 112. We have to calculate the CONTRIBUTIONS to the final resulting CPS, not the direct CPS of the disturbances' PSD.

--- Assignment 3 Part B/B.m
+++ Assignment 3 Part B/B.m
@@ -10,6 +10,7 @@ s = tf('s');                    % Laplace variable
 % Create time vector
 N = size(d,2);                  % Number of samples
 t = linspace(0,(N-1),N)*ts;     % Time vector 
+delta_f = 1/(N*ts);             % Frequency resolution
 
 % Plot signals in time domain
 figure;
@@ -26,46 +27,50 @@ grid on
 % Transform signals to the frequency domain
 fft_d_double = fft(d)/N;            % Obtain the amplitude of d, scale by N
 fft_n_double = fft(n)/N;            % Obtain the amplitude of n, scale by N
-f_vec = (0:N-1)/(N*ts);             % Obtain frequency vector (unshifted)
-f_vec_p = f_vec(1:N/2);             % Frequency vector for positive frequencies only
+f_vec = (0:N/2)/(N*ts);             % Obtain frequency vector (unshifted)
+
+%f_vec_p = f_vec(1:N/2);             % Frequency vector for positive frequencies only
 % Obtain single-sided spectrum
-fft_d = zeros(1,N/2);
-fft_n = zeros(1,N/2);
-% Accumulate to positive frequencies
-fft_d = 2*fft_d_double(1:N/2);
-fft_n = 2*fft_n_double(1:N/2);
-fft_d(1) = fft_d_double(1);         % DC is excluded
-fft_n(1) = fft_n_double(1);         % DC is excluded
+fft_d = fft_d_double(1:N/2+1);
+fft_n = fft_n_double(1:N/2+1);
+fft_d(2:end-1) = 2*fft_d(2:end-1);         % DC and nyq is excluded
+fft_n(2:end-1) = 2*fft_n(2:end-1);         % DC and nyq is excluded
 
 % Plot signals in frequency domain
 figure;
-semilogx(f_vec_p,abs(fft_n),'b',"LineWidth",2)
+semilogx(f_vec,abs(fft_n),'b',"LineWidth",2)
 title("Discrete Fourier Transform of Output Disturbance")
 xlabel("f (Hz)")
 ylabel("|n| (\mum)")
 grid on
 
 figure;
-semilogx(f_vec_p,abs(fft_d),'r',"LineWidth",2)
+semilogx(f_vec,abs(fft_d),'r',"LineWidth",2)
 title("Discrete Fourier Transform of Process Disturbance")
 xlabel("f (Hz)")
 ylabel("|d| (V)")
 grid on
 
 %% B2
+% Compute double-sided PSD
+psd_d_double = (abs(fft(d)).^2)/(N*fs);    % Double-sided PSD
+psd_n_double = (abs(fft(n)).^2)/(N*fs);    % Double-sided PSD
 % Compute single-sided PSD
-psd_d = (abs(fft_d).^2)/(N*fs/2);    % Single-sided PSD
-psd_n = (abs(fft_n).^2)/(N*fs/2);    % Single-sided PSD
+% Compute double-sided PSD
+psd_d = psd_d_double(1:N/2+1);    % Single-sided PSD
+psd_d(2:end-1) = 2*psd_d(2:end-1);
+psd_n = psd_n_double(1:N/2+1);    % Single-sided PSD
+psd_n(2:end-1) = 2*psd_n(2:end-1);
 % Plot the results
 figure;
-semilogx(f_vec_p,psd_d,'r',"LineWidth",2);
+semilogx(f_vec,psd_d,'r',"LineWidth",2);
 title("PSD of Process Disturbance")
 xlabel("f (Hz)")
 ylabel("PSD (V^2/Hz)")
 grid on
 
 figure;
-semilogx(f_vec_p,psd_n,'b',"LineWidth",2);
+semilogx(f_vec,psd_n,'b',"LineWidth",2);
 title("PSD of Output Disturbance")
 xlabel("f (Hz)")
 ylabel("PSD (\mum^2/Hz)")
@@ -78,37 +83,40 @@ P = G;                                  % Define plant tf as P
 GS = P/(1+P*C);                         % Process sensitivity
 S = 1/(1+P*C);                          % Output sensitivity
 % Calculate function frequency response
-[A_GS,f_GS] = freqresp(GS,f_vec_p,'Hz');    % Freqresponse for GS
+[A_GS,~] = freqresp(GS,2*pi*f_vec);    % Freqresponse for GS
 A_GS = squeeze(A_GS)';                      % Remove empty dimensions
-[A_S,f_S] = freqresp(S,f_vec_p,'Hz');       % Freqresponse for S
+[A_S,~] = freqresp(S,2*pi*f_vec);       % Freqresponse for S
 A_S = squeeze(A_S)';                        % Remove empty dimensions
+% Calculate PSD components
+H2_d = abs(A_GS).^2;
+H2_n = abs(A_S).^2;
 % Calculate PSD of y
-psd_y = ((abs(A_GS).^2).*psd_d) + ((abs(A_S).^2).*psd_n);   % PSD of y, the result of this does not make sense
+psd_y = (H2_d.*psd_d) + (H2_n.*psd_n);   % PSD of y, the result of this does not make sense
 % Plot PSD of y
 figure;
-semilogx(f_vec_p,psd_y,'k',"LineWidth",2);
+semilogx(f_vec,psd_y,'k',"LineWidth",2);
 title("PSD of Output y")
 xlabel("f (Hz)")
 ylabel("PSD (\mum^2/Hz)")
 grid on
 figure
-loglog(f_vec_p,abs(A_S))
+loglog(f_vec,abs(A_S))
 title("Response of S")
 figure
-loglog(f_vec_p,abs(A_GS))
+loglog(f_vec,abs(A_GS))
 title("Response of GS")
 %% B4
 % Calculate Cumulative Power Spectrum
-cps_y = cumsum(psd_y.*f_vec_p(2));
-cps_d = cumsum(psd_d.*f_vec_p(2));
-cps_n = cumsum(psd_n.*f_vec_p(2));
+cps_y = cumsum(psd_y)*delta_f;
+cps_d = cumsum(H2_d.*psd_d)*delta_f;
+cps_n = cumsum(H2_n.*psd_n)*delta_f;
 % Plot PSD of y
 figure;
-loglog(f_vec_p,cps_y,'k',"LineWidth",2);
+loglog(f_vec,cps_y,'k',"LineWidth",2);
 hold on
-loglog(f_vec_p,cps_d,'r',"LineWidth",2);
+loglog(f_vec,cps_d,'r',"LineWidth",2);
 hold on
-loglog(f_vec_p,cps_n,'b',"LineWidth",2);
+loglog(f_vec,cps_n,'b',"LineWidth",2);
 hold off
 title("CPS")
 xlabel("f (Hz)")
